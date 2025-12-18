1fb8c1b2f2af63d980d8df87c430506b0fb54584
arifulbasha3-ops <arifulbasha3@gmail.com>
2025-12-18 00:21:21 +0000

    19

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,10 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.accountmanager',
   appName: 'Account Manager',
-  webDir: 'dist',
-  android: {
-    fullscreen: false
-  }
+  webDir: 'dist'
 };
 
 export default config;
